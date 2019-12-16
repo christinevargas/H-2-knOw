1ee626a30033524a7554b021fb4ad1b61859b842
Christine Vargas <christineandreavargas@gmail.com>
2019-12-16 13:22:26 -0600

    Events data and visualization

--- src/components/Calendar.js
+++ src/components/Calendar.js
@@ -3,66 +3,8 @@ import Header from "./Header";
 import React from "react";
 import FullCalendar from "@fullcalendar/react";
 import dayGridPlugin from "@fullcalendar/daygrid";
+import events from "../db/data";
 import "../main.scss";
-
-let events = [
-  {
-    title: "Fiddle Leaf Fig",
-    allDay: true,
-    daysOfWeek: [1],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-10",
-    color: "#339966"
-  },
-  {
-    title: "Bird's Nest Fern",
-    allDay: true,
-    daysOfWeek: [1, 4],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-13",
-    color: "#339966"
-  },
-  {
-    title: "Croton",
-    allDay: true,
-    daysOfWeek: [1],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-10",
-    color: "#339966"
-  },
-  {
-    title: "Bell Pepper Plant",
-    allDay: true,
-    daysOfWeek: [1, 4],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-13",
-    color: "#339966"
-  },
-  {
-    title: "Strawberry Plant",
-    allDay: true,
-    daysOfWeek: [1, 4],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-13",
-    color: "#339966"
-  },
-  {
-    title: "Spider Plant",
-    allDay: true,
-    daysOfWeek: [1],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-10",
-    color: "#339966"
-  },
-  {
-    title: "Wavy Fern",
-    allDay: true,
-    daysOfWeek: [1, 3, 5],
-    startRecur: "2019-12-16",
-    endRecur: "2020-03-14",
-    color: "#339966"
-  }
-];
 
 export default class DemoApp extends React.Component {
   render() {
@@ -77,7 +19,6 @@ export default class DemoApp extends React.Component {
           weekends={false}
           events={events}
         />
-
         <AllPlants />
       </>
     );
---
+++ src/db/data.js
@@ -0,0 +1,312 @@
+let events = [
+  {
+    title: "Fiddle Leaf Fig",
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Bird's Nest Fern",
+    allDay: true,
+    daysOfWeek: [1, 4],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-13",
+    color: "#339966"
+  },
+  {
+    title: "Croton",
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Bell Pepper Plant",
+    allDay: true,
+    daysOfWeek: [1, 4],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-13",
+    color: "#339966"
+  },
+  {
+    title: "Strawberry Plant",
+    allDay: true,
+    daysOfWeek: [1, 4],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-13",
+    color: "#339966"
+  },
+  {
+    title: "Spider Plant",
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Wavy Fern",
+    allDay: true,
+    daysOfWeek: [1, 3, 5],
+    startRecur: "2019-12-16",
+    endRecur: "2020-03-14",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2019-12-17",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-30",
+    endRecur: "2019-12-31",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-13",
+    endRecur: "2020-01-14",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-27",
+    endRecur: "2020-01-28",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-10",
+    endRecur: "2020-02-11",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-24",
+    endRecur: "2020-02-25",
+    color: "#339966"
+  },
+  {
+    title: "Snake Plant",
+    id: 2,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-03-09",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2019-12-17",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-30",
+    endRecur: "2019-12-31",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-13",
+    endRecur: "2020-01-14",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-27",
+    endRecur: "2020-01-28",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-10",
+    endRecur: "2020-02-11",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-24",
+    endRecur: "2020-02-25",
+    color: "#339966"
+  },
+  {
+    title: "Money Tree",
+    id: 3,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-03-09",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2019-12-17",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-30",
+    endRecur: "2019-12-31",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-13",
+    endRecur: "2020-01-14",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-27",
+    endRecur: "2020-01-28",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-10",
+    endRecur: "2020-02-11",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-24",
+    endRecur: "2020-02-25",
+    color: "#339966"
+  },
+  {
+    title: "Dracaena",
+    id: 4,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-03-09",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-16",
+    endRecur: "2019-12-17",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2019-12-30",
+    endRecur: "2019-12-31",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-13",
+    endRecur: "2020-01-14",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-01-27",
+    endRecur: "2020-01-28",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-10",
+    endRecur: "2020-02-11",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-02-24",
+    endRecur: "2020-02-25",
+    color: "#339966"
+  },
+  {
+    title: "Jade",
+    id: 5,
+    allDay: true,
+    daysOfWeek: [1],
+    startRecur: "2020-03-09",
+    endRecur: "2020-03-10",
+    color: "#339966"
+  }
+];
+
+export default events;
